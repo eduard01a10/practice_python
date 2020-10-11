53e634ea5969e9e0aaf0e1a6a4c95241dfc75718
Eduardo Bautista Barrera <eg.bb952@gmail.com>
2020-10-11 16:30:28 -0500

    Solved second excercise

---
+++ P2_odd_or_even.py
@@ -0,0 +1,9 @@
+def run():
+    number = int(input("Write a number: "))
+    if number % 2 == 0:
+        print("Your number is even")
+    else:
+        print("Your number is odd")
+
+if __name__ == "__main__":
+    run()
