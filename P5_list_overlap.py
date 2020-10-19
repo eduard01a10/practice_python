f4a56bc2002e9dea7d78c6d014cb7e4c51d2b98e
Eduardo Bautista Barrera <eg.bb952@gmail.com>
2020-10-19 17:26:11 -0500

    added random list generator

--- P5_list_overlap.py
+++ P5_list_overlap.py
@@ -1,11 +1,15 @@
+import random
+
 def run():
 
-    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 11, 7]
-    b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    randomlist_a = random.sample(range(10, 30), 13)
+    randomlist_b = random.sample(range(10, 30), 17)
+    #a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 11, 7]
+    #b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
     c = []
 
-    for i in b:
-        if a in b:
+    for i in randomlist_b:
+        if randomlist_a in randomlist_b:
             c.append(i)
         print(i)
 
